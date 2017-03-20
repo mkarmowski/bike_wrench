b496ff8e003a208161ef3a4ce24cf6e76e7a8dfd
mkarmowski <mateusz@karmowski.com>
2017-03-20 17:53:15 +0100

    users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'users/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'users/register.html',
+                  {'user_form': user_form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Account is not active')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+@login_required
+def user_details(request):
+    return render(request, 'users/details.html', {'user': request.user})
